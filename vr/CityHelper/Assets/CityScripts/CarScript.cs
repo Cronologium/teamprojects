6e29f7ad2a2e43962fc019905c9cd5be649080d4
Tudor Varan <Cronologium@github.com>
2018-01-16 21:09:29 +0200

    Solved traffic issue

--- vr/CityHelper/Assets/CityScripts/CarScript.cs
+++ vr/CityHelper/Assets/CityScripts/CarScript.cs
@@ -5,53 +5,94 @@ using UnityEngine;
 public class CarScript : MonoBehaviour {
 
     bool isWaiting = false;
+    bool isFirst = false;
     public string direction;
 
-    bool isCarOnTheOppositeLane(GameObject gobj)
+    bool isCarInFront()
     {
-        return gobj.GetComponent<CarScript>().direction[0] == direction[0] && gobj.GetComponent<CarScript>().direction[1] != direction[1];
-    }
-
-    void FixedUpdate()
-    {
-        if (direction != "x-" && direction != "x+")
-            return;
-
-        if (isWaiting)
-        {
-            bool hasNearbyObject = false;
-            foreach (GameObject gobj in GameManager.instance.spawnedCars)
-            {
-                if (gobj == gameObject)
-                    continue;
-
-                if (Vector3.Distance(gameObject.transform.position, gobj.transform.position) < 30f && !isCarOnTheOppositeLane(gobj))
-                {
-                    hasNearbyObject = true;
-                    break;
-                }
-            }
-
-            if (!hasNearbyObject)
-            {
-                isWaiting = false;
-                Vector3 startForce = GameManager.instance.GetForceVectorForDirection(direction);
-                gameObject.GetComponent<Rigidbody>().AddForce(startForce);
-                return;
-            }
-        }
-
         foreach (GameObject gobj in GameManager.instance.spawnedCars)
         {
             if (gobj == gameObject)
                 continue;
 
-            if (Vector3.Distance(gameObject.transform.position, gobj.transform.position) < 30f && !isCarOnTheOppositeLane(gobj))
+            if (gobj.GetComponent<CarScript>().direction == gameObject.GetComponent<CarScript>().direction)
+               
             {
-                isWaiting = true;
-                gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                float value = 100000000f;
+                if (gobj.GetComponent<CarScript>().direction[0] == 'x' && Mathf.Abs(gobj.transform.position.z - gameObject.transform.position.z) < 5)
+                {
+                    value = gobj.transform.position.x - gameObject.transform.position.x;
+                }
+                else if (gobj.GetComponent<CarScript>().direction[0] == 'z' && Mathf.Abs(gobj.transform.position.x - gameObject.transform.position.x) < 5)
+                {
+                    value = gobj.transform.position.z - gameObject.transform.position.z;
+                }
+                if (gobj.GetComponent<CarScript>().direction[1] == '-')
+                {
+                    value = -value;
+                }
+                if (value > 0 && value < 30f)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void FixedUpdate()
+    {
+        if (isWaiting)
+        {
+            if (!isCarInFront())
+            {
+                launchCar(false);
                 return;
             }
         }
+        else
+        {
+            if (isCarInFront())
+            {
+                stopCar(false);
+                return;
+            }
+        }
+        
+    }
+
+    public void stopCar(bool first)
+    {
+        if (first)
+        {
+            isFirst = true;
+        }
+        else
+        {
+            if (isFirst)
+            {
+                return;
+            }
+            isWaiting = true;
+        }
+        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+
+    public void launchCar(bool first)
+    {
+        if (first)
+        {
+            isFirst = false;
+        }
+        else
+        {
+            if (isFirst)
+            {
+                return;
+            }
+            isWaiting = false;
+        }
+        Vector3 startForce = GameManager.instance.GetForceVectorForDirection(direction);
+        gameObject.GetComponent<Rigidbody>().AddForce(startForce);
     }
 }
---
+++ vr/CityHelper/Assets/CityScripts/IntersectionScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntersectionScript : MonoBehaviour {
+
+    public GameObject trigger1;
+    public GameObject trigger2;
+    public GameObject trigger3;
+    public GameObject trigger4;
+
+    public int greenSeconds;
+    public int cooldownSeconds;
+
+    private int framesLeft;
+
+    private bool areAnyAllowed;
+    private bool wasTrigger1Last;
+
+    // Use this for initialization
+    void Start() {
+        areAnyAllowed = false;
+        wasTrigger1Last = false;
+        framesLeft = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (framesLeft > 0)
+        {
+            framesLeft--;
+        }
+        else
+        {
+            Switch();
+        }
+    }
+
+    public void Switch() {
+        Debug.Log("switching " + wasTrigger1Last + " " + areAnyAllowed);
+        if (!areAnyAllowed)
+        {
+            if (!wasTrigger1Last)
+            {
+                trigger1.GetComponent<TriggerScript>().stopCars = false;
+                trigger2.GetComponent<TriggerScript>().stopCars = false;
+                wasTrigger1Last = true;
+                areAnyAllowed = true;
+                framesLeft = (int) (greenSeconds * 1 / Time.deltaTime);
+            }
+            else
+            {
+                trigger3.GetComponent<TriggerScript>().stopCars = false;
+                trigger4.GetComponent<TriggerScript>().stopCars = false;
+                areAnyAllowed = true;
+                wasTrigger1Last = false;
+                framesLeft = (int)(greenSeconds * 1 / Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (!wasTrigger1Last)
+            {
+                trigger3.GetComponent<TriggerScript>().stopCars = true;
+                trigger4.GetComponent<TriggerScript>().stopCars = true;
+                areAnyAllowed = false;
+                wasTrigger1Last = false;
+                framesLeft = (int)(cooldownSeconds * 1 / Time.deltaTime);
+            }
+            else
+            {
+                trigger1.GetComponent<TriggerScript>().stopCars = true;
+                trigger2.GetComponent<TriggerScript>().stopCars = true;
+                areAnyAllowed = false;
+                wasTrigger1Last = true;
+                framesLeft = (int)(cooldownSeconds * 1 / Time.deltaTime);
+            }
+        }
+    }
+}
